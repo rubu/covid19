87fedec184c1901bbc278d271f1a0a8cdef14840
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2022-01-19 14:52:58 +0200

    handle http errors

--- covid19.py
+++ covid19.py
@@ -4,15 +4,19 @@ import urllib.parse
 import json
 
 def execute_sql_query(query):
-    request = urllib.request.urlopen(f'https://data.gov.lv/dati/lv/api/3/action/datastore_search_sql?sql={urllib.parse.quote(query)}')
-    result = json.loads(request.read().decode('utf-8'))
-    if ('success' not in result or result['success'] != True or 'result' not in result):
-        print(f'failed to execute query "${query}", result: result')
+    try:
+        request = urllib.request.urlopen(f'https://data.gov.lv/dati/lv/api/3/action/datastore_search_sql?sql={urllib.parse.quote(query)}')
+        result = json.loads(request.read().decode('utf-8'))
+        if ('success' not in result or result['success'] != True or 'result' not in result):
+            print(f'failed to execute query "${query}", result: result')
+            quit()
+        result = result['result'] 
+        if ('records' in result):
+            return result['records']
+        return []
+    except urllib.error.HTTPError as e:
+        print(e.read().decode('utf-8'))
         quit()
-    result = result['result'] 
-    if ('records' in result):
-        return result['records']
-    return []
 
 data_interval_query = 'SELECT MIN("Vakcinācijas datums"), MAX("Vakcinācijas datums") from "9320d913-a4a2-4172-b521-73e58c2cfe83"'
 data_interval_result = execute_sql_query(data_interval_query)
@@ -29,5 +33,5 @@ if (len(data_interval_result) == 0):
 
 for vaccination_stage in vaccine_stage_result:
     group_size_query = f'SELECT SUM("Vakcinēto personu skaits") from "9320d913-a4a2-4172-b521-73e58c2cfe83" WHERE "Vakcinācijas posms" = "{vaccination_stage["Vakcinācijas posms"]}"'
-    group_size_result = json.loads(execute_sql_query(group_size_query))
+    group_size_result = execute_sql_query(group_size_query)
     print(group_size_result)
