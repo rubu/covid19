a87c32931b69a60e01dcb4ac666bdc5a53f14fd5
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2022-01-19 19:48:37 +0200

    add grand total

--- covid19.py
+++ covid19.py
@@ -38,21 +38,27 @@ if (len(data_interval_result) == 0):
     print('failed to obtain vaccination stages')
     quit()
 
+total = 0
 for vaccine_stage in vaccine_stage_result:
     vaccine_stage_name = vaccine_stage['Vakcinācijas posms']
-    total = 0
+    stage_total = 0
     vaccine_type_totals = []
     for vaccine_type in vaccine_type_result:
         vaccine_type_name = vaccine_type['Preparāts']
         vaccine_type_group_size_query = f'SELECT SUM("Vakcinēto personu skaits") from "9320d913-a4a2-4172-b521-73e58c2cfe83" WHERE "Vakcinācijas posms" = \'{vaccine_stage_name}\' and "Preparāts" = \'{vaccine_type_name}\''
         vaccine_type_group_size_result = execute_sql_query(vaccine_type_group_size_query)
         if vaccine_type_group_size_result[0]["sum"] != None:
-            total += int(vaccine_type_group_size_result[0]["sum"])
+            stage_total += int(vaccine_type_group_size_result[0]["sum"])
             vaccine_type_totals.append({'type': vaccine_type_name, 'sum': vaccine_type_group_size_result[0]['sum']})
     vaccine_group_size_query = f'SELECT SUM("Vakcinēto personu skaits") from "9320d913-a4a2-4172-b521-73e58c2cfe83" WHERE "Vakcinācijas posms" = \'{vaccine_stage_name}\''
-    total_from_database = int(execute_sql_query(vaccine_group_size_query)[0]['sum'])
-    if total_from_database != total:
-        print(f'!!! math does not add up for "{vaccine_stage_name}", calculated total: {total}, total from api: {total_from_database} !!!')
-    print(f'{vaccine_stage_name}: {total}')
+    stage_total_from_database = int(execute_sql_query(vaccine_group_size_query)[0]['sum'])
+    if stage_total_from_database != stage_total:
+        print(f'!!! math does not add up for "{vaccine_stage_name}", calculated total: {stage_total}, total from api: {stage_total_from_database} !!!')
+    print(f'{vaccine_stage_name}: {stage_total}')
     for vaccine_type_total in vaccine_type_totals:
-        print(f'\t{vaccine_type_total["type"]}: {vaccine_type_total["sum"]} ({round(int(vaccine_type_total["sum"]) * 100 / total, 2)}%)')
+        print(f'\t{vaccine_type_total["type"]}: {vaccine_type_total["sum"]} ({round(int(vaccine_type_total["sum"]) * 100 / stage_total, 2)}%)')
+    total += stage_total
+print(f'total vaccinations: {total}')
+#eligible_people_count_query = 'SELECT * from "83da473d-68e1-4120-93f4-817bafce88a2"'
+#eligible_people_count_result = execute_sql_query(eligible_people_count_query)
+#print(eligible_people_count_result)
