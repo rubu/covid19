8a9c29174e57c56e7842fc25789a254178209ee5
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2022-01-19 14:47:04 +0200

    add fetching vaccination groups

--- covid19.py
+++ covid19.py
@@ -4,28 +4,30 @@ import urllib.parse
 import json
 
 def execute_sql_query(query):
-    request = urllib.request.urlopen(f'https://data.gov.lv/dati/lv/api/3/action/datastore_search_sql?sql={urllib.parse.quote(query)}')    
-    return request.read().decode('utf-8')
+    request = urllib.request.urlopen(f'https://data.gov.lv/dati/lv/api/3/action/datastore_search_sql?sql={urllib.parse.quote(query)}')
+    result = json.loads(request.read().decode('utf-8'))
+    if ('success' not in result or result['success'] != True or 'result' not in result):
+        print(f'failed to execute query "${query}", result: result')
+        quit()
+    result = result['result'] 
+    if ('records' in result):
+        return result['records']
+    return []
 
 data_interval_query = 'SELECT MIN("Vakcinācijas datums"), MAX("Vakcinācijas datums") from "9320d913-a4a2-4172-b521-73e58c2cfe83"'
-data_interval_result = json.loads(execute_sql_query(data_interval_query))
-if ('success' not in data_interval_result or data_interval_result['success'] != True or 'result' not in data_interval_result):
-    print(f'failed to obtain start and end timestamps of the data collection, response: {data_interval_result}')
+data_interval_result = execute_sql_query(data_interval_query)
+if (len(data_interval_result) == 0):
+    print('failed to obtain vaccination time range')
+    quit()
+print(f'results range from {data_interval_result[0]["min"]} to {data_interval_result[0]["max"]}')
+
+vaccine_stage_query = 'SELECT DISTINCT "Vakcinācijas posms" from "9320d913-a4a2-4172-b521-73e58c2cfe83"'
+vaccine_stage_result = execute_sql_query(vaccine_stage_query)
+if (len(data_interval_result) == 0):
+    print('failed to obtain vaccination stages')
     quit()
 
-result = data_interval_result['result']
-if ('records' not in result):
-    print('invalid result format')
-    quit()
-
-records = result['records']
-if (len(records) == 0):
-    print('result does not contain any records')
-    quit()
-
-date_interval = records[0]
-print(f'results range from {date_interval["min"]} to {date_interval["max"]}')
-
-group_size_query = 'SELECT COUNT("Vakcinēto personu skaits") from "9320d913-a4a2-4172-b521-73e58c2cfe83" GROYP BY("Vakcinācijas posms")'
-group_size_result = json.loads(execute_sql_query(group_size_query))
-print(group_size_result)
+for vaccination_stage in vaccine_stage_result:
+    group_size_query = f'SELECT SUM("Vakcinēto personu skaits") from "9320d913-a4a2-4172-b521-73e58c2cfe83" WHERE "Vakcinācijas posms" = "{vaccination_stage["Vakcinācijas posms"]}"'
+    group_size_result = json.loads(execute_sql_query(group_size_query))
+    print(group_size_result)
